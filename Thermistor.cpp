86420536b573ad271dee7ba3b505e4d13701449d
username115 <username115@users.noreply.github.com>
2015-11-10 18:29:32 -0500

    Added more features, cleaned up a bit

--- Thermistor.cpp
+++ Thermistor.cpp
@@ -7,12 +7,15 @@
 
 #include "Thermistor.h"
 
-Thermistor::Thermistor(unsigned int analog_pin, unsigned int scale_ohm, unsigned int static_ohm, float temp_nom_c, unsigned int b_coeff) {
+Thermistor::Thermistor(unsigned int analog_pin, unsigned int scale_ohm,
+		unsigned int static_ohm, float temp_nom_c, unsigned int b_coeff) {
 	m_Pin = analog_pin;
 	m_ScaleOhm = scale_ohm;
 	m_StaticOhm = static_ohm;
 	m_NomC = temp_nom_c;
 	m_BetaCoeff = b_coeff;
+	for (int i = 0; i < MAX_RECORDED_TEMPS; i++)
+		m_lastTempC[i] = -273.15f;
 }
 
 Thermistor::~Thermistor() {
@@ -24,11 +27,10 @@ float Thermistor::readTemp(Temperature temp) {
 
 	reading = analogRead(m_Pin);
 
-	reading = (1023 / reading)  - 1;
+	reading = (1023 / reading) - 1;
 	reading = m_StaticOhm / reading;
 
-
-	float steinhart;
+	float steinhart;			     // Shamelessly stolen from Adafruit
 	steinhart = reading / m_ScaleOhm;            // (R/Ro)
 	steinhart = log(steinhart);                  // ln(R/Ro)
 	steinhart /= m_BetaCoeff;                    // 1/B * ln(R/Ro)
@@ -36,10 +38,37 @@ float Thermistor::readTemp(Temperature temp) {
 	steinhart = 1.0 / steinhart;                 // Invert
 	steinhart -= 273.15;                         // convert to C
 
+	for (int i = MAX_RECORDED_TEMPS - 1; i > 0; i--) {
+		m_lastTempC[i] = m_lastTempC[i - 1];
+	}
+	m_lastTempC[0] = steinhart;
+
 	if (temp == FAHRENHEIT) {
-		steinhart = (steinhart*9/5)+32;
+		steinhart = (steinhart * 9 / 5) + 32;
 	}
 
 	return steinhart;
+}
+
+float Thermistor::lastTemp(Temperature temp) {
+	if (temp == FAHRENHEIT) {
+		return (m_lastTempC[0] * 9 / 5) + 32;
+	}
+	return m_lastTempC[0];
+}
+
+float Thermistor::average3Temp(Temperature temp) {
+	float avg = 0;
+	int count = MAX_RECORDED_TEMPS;
+	for(int i=0; i<MAX_RECORDED_TEMPS; i++)
+		if (m_lastTempC[i] > -273.0)
+			avg += m_lastTempC[i];
+		else
+			count--;
+	avg /= count;
+	if (temp == FAHRENHEIT) {
+		return (avg * 9 / 5) + 32;
+	}
+	return avg;
 }
 
--- Thermistor.h
+++ Thermistor.h
@@ -9,14 +9,28 @@
 #define THERMISTOR_H_
 #include "Arduino.h"
 
+#define THERM_NOM_RES 3000
+#define STATIC_RES 10000
+#define NOM_TEMP_C 25.0
+#define DEFAULT_BETA_COEFF 3936
+#define MAX_RECORDED_TEMPS 3
+
+// Beta is calculated using two known resistance-temperature values. equation is as follows: (ln(R/R0))/((1/(T+273.15))-(1/(T0+273.15))) where resistance is in ohms, and tmperature is in Celcius
+
 class Thermistor {
-	enum Temperature{CELCIUS, FAHRENHEIT};
+	enum Temperature {
+		CELCIUS, FAHRENHEIT
+	};
 public:
 
-	Thermistor(unsigned int analog_pin, unsigned int scale_ohm=3000, unsigned int static_ohm=10000, float temp_nom_c = 25.0, unsigned int b_coeff = 3936);
-	virtual ~Thermistor();
+	Thermistor(unsigned int analog_pin, unsigned int scale_ohm = THERM_NOM_RES,
+			unsigned int static_ohm = STATIC_RES, float temp_nom_c = NOM_TEMP_C,
+			unsigned int b_coeff = DEFAULT_BETA_COEFF);
+	~Thermistor();
 
-	virtual float readTemp(Temperature temp = CELCIUS);
+	float readTemp(Temperature temp = CELCIUS);
+	float lastTemp(Temperature temp = CELCIUS);
+	float average3Temp(Temperature temp = CELCIUS);
 
 private:
 	unsigned int m_ScaleOhm;
@@ -24,6 +38,8 @@ private:
 	unsigned int m_Pin;
 	float m_NomC;
 	unsigned int m_BetaCoeff;
+
+	float m_lastTempC[MAX_RECORDED_TEMPS];
 
 };
 
